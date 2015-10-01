5ef44a7e598d9aab36c558b7dd43a14c5dc64605
Darius Chow <dariuschow@gmail.com>
2015-10-01 13:50:12 -0400

    changed comments and cleaned up variables

--- exercise1.py
+++ exercise1.py
@@ -14,11 +14,15 @@ __license__ = "MIT License"
 
 # to do: make assumptions explicit
 # for example, assumption that commission is paid on top of money spent on stock
+
+# to do: add scenario to describe the test case.
+
+#Constants
 commission = 0.03
-
-
 shares = 2000
 sale_price = 900.00
+sell_price = 942.75
+
 # calculate money spent 2000 shares * $900.00 per share
 money_spent = shares * sale_price
 # calculate commission as 3% of money spent
@@ -26,21 +30,21 @@ money_spent_plus_commission = money_spent + (money_spent * commission)
 # store money spent
 start_money = money_spent_plus_commission
 # print money
+# this is negative because the cash balance of Lakshmi is negative after purchasing stock
 print(0 - money_spent_plus_commission)
 # calculate money earned as 2000 shares * $942.75 per share
-purchase_price = 942.75
-money_earned = shares*purchase_price
+money_earned = shares * sell_price
 # calculate commission as 3% of money earned
 # subtract commission from money earned
 money_earned_minus_commission = money_earned - (money_earned * commission)
-# calculate final balance
-# compare money earned to money spent to determine whether Lakshmi made a profit or loss
-end_money = money_earned_minus_commission - money_spent_plus_commission
+# calculate final balance by comparing money earned to money spent to
+#   determine whether Lakshmi made a profit or loss
+end_money = money_earned_minus_commission - start_money
 # print money
 print (end_money)
 
 
 #placeholder
-money = 0
+money = ""
 
 print(money)
