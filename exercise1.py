5d77e7a68260f3c050e9a3fb80fd620d3333a843
eden-r <e.rusnell@mail.utoronto.ca>
2015-10-01 12:30:06 -0400

    second transaction (sell)

--- exercise1.py
+++ exercise1.py
@@ -14,23 +14,30 @@ __license__ = "MIT License"
 
 
 commission = 0.03
-purchase_price = 942.75
+
 
 shares = 2000
 sale_price = 900.00
 # calculate money spent 2000 shares * $900.00 per share
 money_spent = shares * sale_price
 # calculate commission as 3% of money spent
-money_spent_plus_commission = money_spent * commission
+money_spent_plus_commission = money_spent + (money_spent * commission)
 # store money spent
 start_money = money_spent_plus_commission
 # print money
 print(0 - money_spent_plus_commission)
 # calculate money earned as 2000 shares * $942.75 per share
+purchase_price = 942.75
+money_earned = shares*purchase_price
 # calculate commission as 3% of money earned
 # subtract commission from money earned
-# print money
+money_earned_minus_commission = money_earned - (money_earned * commission)
+# calculate final balance
 # compare money earned to money spent to determine whether Lakshmi made a profit or loss
+end_money = money_earned_minus_commission - money_spent_plus_commission
+# print money
+print (end_money)
+
 
 #placeholder
 money = 0
