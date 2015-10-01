beb66c9d0a4efba1fad09ecd08b40b45c0144342
Darius Chow <dariuschow@gmail.com>
2015-10-01 00:38:38 -0400

    Edited comments to exercise 1

--- exercise1.py
+++ exercise1.py
@@ -15,14 +15,16 @@ __license__ = "MIT License"
 
 money = 1000.00
 
-# calculate money spent 2000*900.00
+# calculate money spent 2000 shares * $900.00 per share
+# store money spent
 # calculate commission as 3% of money spent
-# subtract money spent + commission from money
+# subtract commission from money spent and store as money
 # print money
-# calculate money earned as 2000*942.75
+# calculate money earned as 2000 shares * $942.75 per share
 # calculate commission as 3% of money earned
-# subtract commission from money
-# add money earned to money
+# subtract commission from money earned
 # print money
+# compare money earned to money spent to determine whether Lakshmi made a profit or loss
+
 
 print(money)
