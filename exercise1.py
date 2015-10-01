9fa502c0eb4051ec9788bd12815867c656fb0e03
eden-r <e.rusnell@mail.utoronto.ca>
2015-09-30 21:35:09 -0400

    first revision - pseudocode

--- exercise1.py
+++ exercise1.py
@@ -14,4 +14,15 @@ __license__ = "MIT License"
 
 
 money = 1000.00
+
+# calculate money spent 2000*900.00
+# calculate commission as 3% of money spent
+# subtract money spent + commission from money
+# print money
+# calculate money earned as 2000*942.75
+# calculate commission as 3% of money earned
+# subtract commission from money
+# add money earned to money
+# print money
+
 print(money)
