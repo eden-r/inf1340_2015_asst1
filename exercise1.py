8439f5f113ccef6c0ae3d0d2d20629dc5eb86b19
eden-r <e.rusnell@mail.utoronto.ca>
2015-10-04 20:25:58 -0400

    added assumptions / removed pseudocode

--- exercise1.py
+++ exercise1.py
@@ -23,24 +23,18 @@ shares = 2000
 sale_price = 900.00
 sell_price = 942.75
 
-# calculate money spent 2000 shares * $900.00 per share
 money_spent = shares * sale_price
-# calculate commission as 3% of money spent
 money_spent_plus_commission = money_spent + (money_spent * commission)
-# store money spent
 start_money = money_spent_plus_commission
-# print money
+
 # this is negative because the cash balance of Lakshmi is negative after purchasing stock
 print(0 - money_spent_plus_commission)
-# calculate money earned as 2000 shares * $942.75 per share
+
 money_earned = shares * sell_price
-# calculate commission as 3% of money earned
-# subtract commission from money earned
 money_earned_minus_commission = money_earned - (money_earned * commission)
-# calculate final balance by comparing money earned to money spent to
-#   determine whether Lakshmi made a profit or loss
 end_money = money_earned_minus_commission - start_money
-# print money
+
+# end money is the result of the first two transactions combined
 print (end_money)
 
 
