536b00f50c64b89a4389ce2f17b6b30c5a25e5c1
eden-r <e.rusnell@mail.utoronto.ca>
2015-10-04 20:31:03 -0400

    added first if statements

--- exercise3.py
+++ exercise3.py
@@ -27,36 +27,28 @@ def diagnose_car():
     """
 
     question_one = raw_input("Is the car silent when you turn the key?")
-
-    question_two = raw_input("Are the battery terminals corroded?") # if Y to question 1
+    if question_one == "y":
+        question_two = raw_input("Are the battery terminals corroded?") # if Y to question 1
     print("Clean terminals and try starting again.") # if Y to question 2
     print("Replace cables and try again.") #if N to question 2
 
-    question_three = raw_input("Does the car make a clicking noise?") # if N to question 1
+    if question_one == "n":
+        question_three = raw_input("Does the car make a clicking noise?") # if N to question 1
 
     print("Replace the battery.") # if Y to question 3
 
     question_four = raw_input("Does the car crank up but fail to start?") # if N to quesiton 3
-
     print("Check spark plug connection.") #if Y to question 4
 
     question_five = raw_input("Does the engine start and then die?") # if N to question 4
 
     question_six = raw_input("Does your car have fuel injection?") # if Y to question 5
-
     print("Your car is working.") # if N to question 5
-
     print("Check to ensure the choke is opening and closing.") #if Y to question 6
-
     print("Get it in for service.") # if N to question 6
 
     print("Error.") # if something other than y or N is entered
 
-
-
-
-
-    print("The battery cables may be damaged. Replace cables and try again.")
 
 
 
